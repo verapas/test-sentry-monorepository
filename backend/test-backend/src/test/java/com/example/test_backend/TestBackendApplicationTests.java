1df80edf6dcfa19b71f14fe79cbd4b3a1dd11802
PascalVeraguth <pascal.veraguth@cyrotech.ch>
2025-10-16 11:46:40 +0200

    use pnpm instead of npm. add @nxrocks/nx-spring-boot. add test-frontend and a test-backend

---
+++ backend/test-backend/src/test/java/com/example/test_backend/TestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- frontend/test-frontend/eslint.config.mjs
+++ frontend/test-frontend/eslint.config.mjs
@@ -1,25 +1,8 @@
-import baseConfig from './eslint.base.config.mjs';
 import nx from '@nx/eslint-plugin';
+import baseConfig from '../../eslint.base.config.mjs';
 
 export default [
   ...baseConfig,
-  {
-    ignores: ['**/dist'],
-  },
-  {
-    files: [
-      '**/*.ts',
-      '**/*.tsx',
-      '**/*.cts',
-      '**/*.mts',
-      '**/*.js',
-      '**/*.jsx',
-      '**/*.cjs',
-      '**/*.mjs',
-    ],
-    // Override or add rules here
-    rules: {},
-  },
   ...nx.configs['flat/angular'],
   ...nx.configs['flat/angular-template'],
   {
